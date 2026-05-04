8138c409ac7f6e7fd0e25e551b4315346a22a596
Luciano Federicci <lucianofedericci@gmail.com>
2026-05-03 21:49:58 -0300

    chore: cleaned project name and updated dependencies
    - deleted mysql password from backend for easier testing. Using xampp

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,12 +3,9 @@ import { ThemeProvider } from "@mui/material/styles";
 import { router } from "./routes";
 import { theme } from "./theme";
 
-export default function App(props: any) {
-  // Filter out Figma-specific props that are not supported by MUI ThemeProvider
-  const { 'data-fg-d3bl0': _fg, 'data-fgid-d3bl0': _fgid, ...cleanProps } = props;
-  
+export default function App() {
   return (
-    <ThemeProvider theme={theme} {...cleanProps}>
+    <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
     </ThemeProvider>
   );
